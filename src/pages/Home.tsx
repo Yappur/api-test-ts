7ff34ea626533b4995e9ee84e052505dab79a520
Mateo <mateox463@gmail.com>
2025-08-20 04:50:05 -0300

    first GET of information

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
-     const {name} = useParams();
+
+const [movies, setMovies] = useState();
+
+     const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+     const popularMovies = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&language=en-US&page=1";
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+      useEffect(() => {
+    if (movies) {
+      console.log(movies);
+    }
+  }, [movies]);
+
+     const fetchData = async () => {
+        axios.get(popularMovies).then((response) => {
+            setMovies(response.data.results);
+        });
+     }
   return (
    
     <div>
       <h1>Home</h1>
-      <p>{name}</p>
+      
     </div>
   );
 }
