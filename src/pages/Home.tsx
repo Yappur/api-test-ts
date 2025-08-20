76ade4da1554e5ec755a577938376221718c8671
Mateo <mateox463@gmail.com>
2025-08-20 05:10:02 -0300

    mapping of the request

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+interface Movies {
+    id: number;
+    title: string;
+    poster_path: string;
+    release_date: number;
+
+}
+
 function Home() {
 
-const [movies, setMovies] = useState();
+const [movies, setMovies] = useState <Movies[]>([]);
 
      const apiKey = import.meta.env.VITE_TMDB_API_KEY;
      const popularMovies = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&language=en-US&page=1";
@@ -11,12 +19,6 @@ const [movies, setMovies] = useState();
     useEffect(() => {
         fetchData();
     }, []);
-
-      useEffect(() => {
-    if (movies) {
-      console.log(movies);
-    }
-  }, [movies]);
 
      const fetchData = async () => {
         axios.get(popularMovies).then((response) => {
@@ -26,7 +28,14 @@ const [movies, setMovies] = useState();
   return (
    
     <div>
-      <h1>Home</h1>
+        <h1>Popular Movies</h1>
+     {movies.map((movie) => (
+        <div key={movie.id}>
+<h2>{movie.title}</h2>
+<img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt={movie.title} width={200} />
+<p>Lanzamiento: {movie.release_date}</p>
+        </div>
+     ))}
       
     </div>
   );
